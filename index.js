cc14c4022eb2fcbe2ab87f3f0735b3dc7d807b3a
sh4faq <hamzehemriesh920@gmail.com>
2025-11-23 16:59:24 -0500

    Update port for production deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const db = require('./queries')
-const port = 3001
+const port = process.env.PORT || 3001
 
 app.use(cors())
 app.use(bodyParser.json())
